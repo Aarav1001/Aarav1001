f7bf2ff889b87f10e5d9b3dfc2ba34af5d4879ff
Aarav1001 <147985854+Aarav1001@users.noreply.github.com>
2023-10-15 13:46:54 +0100

    Create Game.py
    School porject

---
+++ Game.py
@@ -0,0 +1,71 @@
+import time
+
+name = input('what would you like your name to be? ')
+print('hello ' +name)
+age = int(input('how old are you? '))
+
+if age < 8:
+    time.sleep(5)
+    print('This is the story of our land, the metorpolitan republic of gia.')
+    print('it all started one day, on the day of the end of our leader, he died to unknown reasons.')
+    print('that day,')
+    time.sleep(3)
+    print('that day, was the end of the third era of our republic.')
+    print('we were a republic on the planet gia, neighbouring planet of Earth, full of people who did not like us')
+    print('then at the death of our leader, they lost respect for us, and wanted to invade us, we were left defencless!')
+    time.sleep(5)
+    print('the things that they did to them...')
+    time.sleep(5)
+    print('they were unspeakable, so unspeakable, i can not tell you until you are older')
+    print('however, there was one last strand of hope, the third leaders incrarnation, the one born after his death...')
+
+elif age > 8:
+    print('This is the story of our land, the metorpolitan republic of gia.')
+    print('it all started one day, on the day of the end of our leader, he died due to his assassination')
+    time.sleep(5)
+   
+    print('he was assassinated by the people of the neighbouring planet of Earth.')
+   
+    time.sleep(5)
+    print(""" the people of Earth because of an event that happened due to the war of eslovia,
+were our people didn't want to help the earthlings due to the fact that we were not as advanced, and they didn't take it niceley""")
+   
+    print('the only reason they did not declare war on us was because of out thrid leader, the nameless one')
+    time.sleep(10)
+    print('he was able to stop what happened, as when they declared war on us for the first time...')
+    time.sleep(5)
+    print('he was able to stop them single handedey.')
+    time.sleep(7)
+    print('this made them gain respect for him, but when he died...')
+    time.sleep(3)
+    print('it was said the person bron on the excact day of his death, would be his reincarnation.')
+    time.sleep(11)
+    print('what happened after you ask?')
+    time.sleep(5)
+    print('well, the leader of earth declared war once more.')
+    time.sleep(7)
+    print('unspeakable things happened to those captured by them.')
+    time.sleep(7)
+    print('they were sent to concentration camps, where they were forced to work, some were left on the homeland of gia, whilst others...')
+    time.sleep(5)
+    print(""" other weren't so lucky, they would be sent to labour camps on earth to work for the rest of their lives,
+whilst there family was held hostage""")
+    print('however, there was one last strand of hope, the third leaders incrarnation, the one born after his death...')
+time.sleep(10)
+
+
+print('you...')
+
+time.sleep(3)
+print('you, were the only one born on the day.')
+time.sleep(1)
+print('the day of the death of our leader.')
+
+print('now that you have heard the story of your people,')
+time.sleep(3)
+question = input('now, would you like to be born, or wait some more time? ')
+
+if question == 'yes':
+    print('very nice, now lets start')
+elif queston == 'no':
+    print('okay then')
